241cc9ee0113f51143882be8fd6354bc895746e2
norafk <noracai0625@gmail.com>
2024-11-28 11:46:06 -0500

    Configure Gunicorn for Heroku

--- server/app.py
+++ server/app.py
@@ -89,4 +89,4 @@ def process_message():
 #     app.run(debug=True, port=8080)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=int(os.environ.get("PORT", 8080)), host="0.0.0.0")
+    app.run(debug=False, port=int(os.environ.get("PORT", 8080)), host="0.0.0.0")
